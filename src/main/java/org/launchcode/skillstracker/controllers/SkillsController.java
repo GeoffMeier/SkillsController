102f922f5a63c0aac4fd0973eae4511bd01e7f90
Geoffrey Meier <grm0809@gmail.com>
2023-02-07 11:31:14 -0600

    Studio

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class SkillsController {
     @RequestMapping(value="/favorite", method =  {RequestMethod.POST, RequestMethod.GET} )
-    @GetMapping()
+    @GetMapping("/favorite")
     @ResponseBody
     public String favoriteLanguages(@RequestParam String name, @RequestParam String one, @RequestParam String two, @RequestParam String three){
         return "<html>"+
                 "<body>"+
-                "<h1> Name: " + name + "/h1>" +
+                "<h1> Name: " + name + "</h1>" +
                "<ol>"+ "<li>" + one + "</li>" + "<li>" + two + "</li>" + "<li>" + three + "</li>"+
                 "</body>"+
                 "</html>";
@@ -37,13 +37,13 @@ public class SkillsController {
         return "<html>" +
                 "<body>" +
                 "<form action=  'favorite' method= 'post'>" + // submit request to /hello
-                "<h3>Enter Your Name:  <input type = 'text' name='name'></h3>"+
+                "<h3 name='name'>Enter Your Name:  <input type = 'text' name = 'name'></h3>"+
                 "<h3> My Favorite Language is  </h3>" +
-                "<select>"  + " <option>Java</option>  <option>JavaScript</option> <option>CSS</option></select>" +
+                "<select name = 'one'> <option>Java</option>  <option>JavaScript</option> <option>CSS</option></select>" +
                 "<h3>My 2nd Favorite Language is:  </h3>" +
-                "<select name = 'two' > <option>Java</option>  <option>JavaScript</option> <option>CSS</option></select>" +
+                "<select name = 'two' > <option>Java</option>  <option>JavaScript</option> <option>CSS</option> </select>" +
                 "<h3>My 3rd Favorite Language is:  </h3>" +
-               " <select name= ' three' > <option>Java</option>  <option>JavaScript</option> <option>CSS</option></select>"  +
+               " <select name= 'three' > <option>Java</option>  <option>JavaScript</option> <option>CSS</option> </select>"  +
                 " <input type= 'submit' value='Submit'>" +
                 "</form>" +
                 "</body>" +
